4dbb37cf01a5a770dcc254efa1d9320c5abe8f6c
salvador salazar <salvador.g.salazar79@gmail.com>
2021-10-13 10:59:39 -0500

    hellowWorld servlets exercise

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -1,9 +1,79 @@
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.rmi.ServerException;
+
+//Hey tomcast , whwenever someone requests that url /hello , load and execute this servlet
+//the webservelt annotation maps a url to a servlet and its code
+@WebServlet(name = "HelloWorldServlet", urlPatterns = "/hello")
 
 public class HelloWorldServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest req, HttpServletResponse res) {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServerException, IOException {
+        res.setContentType("text/html");
+        PrintWriter out = res.getWriter();
+        out.println("<h1 ><center>Hello, World!</center></h1>");
+
+        String name = req.getParameter("name");
+        out.println("<h1>Hello , " + name + "</h1>");
+        out.println("<hr>");
+        out.println("<h2 style='color:red'>Change font color of HTML webpage tag</h2>");
+//        out.println("<p> Enter : <h2 style='color:red'> ....</h2> </p>");
+
+
+        out.println("<hr>");
+
+
+
+        out.println("<hr>");
+        out.println("<h2> Bonus\n" +
+                "\n" +
+                "Make the page say \"Hello, <name>!\" if name is passed as part of the query string (e.g. /hello?name=codeup), otherwise default to \"Hello, World!\".</h2>");
+
+        out.println("<hr>");
+
+
 
     }
 }
+
+
+
+
+
+
+
+
+
+//public class HelloWorldServlet extends HttpServlet {
+//    protected void doGet(HttpServletRequest req, HttpServletResponse res) {
+//
+//    }
+
+//
+//@WebServlet(name = "HelloWorldServlet", urlPatterns = "/hello-world")
+//
+//public class HelloWorldServlet extends HttpServlet {
+//    protected void doGet(HttpServletRequest req, HttpServletResponse res)throws ServletException, IOException {
+//        response.setContentType("text/html");
+//        PrintWriter out = response.getWriter();;
+//        out.println("<h1> Hello, World!</h1>");
+//
+//    }
+
+
+//
+// out.println("<h2 style ='background-color:coral'> @WebServlet(name = \"HelloWorldServlet\", urlPatterns = \"/\")\n" +
+//         "\n" +
+//         "public class HelloWorldServlet extends HttpServlet {" +
+//         "    @Override" +
+//         "    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServerException, IOException {\n" +
+//         "        res.setContentType(\"text/html\");"+
+//
+//         "        out.println(\"<h1 style ='background-color:coral' >Hello, World!</h1>\");" +
+//         "        out.println(\"<h2> \");" +
+//
+//         "    }");
